b62c1891bd5077a60e926b60712e868ce0429b5f
grokzomborg <robisonpedroso0089@gmail.com>
2026-03-30 05:41:45 -0300

    Create setup.py with basic configuration for GrokZomborg project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GrokZomborg',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A Python package for GrokZomborg project.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
